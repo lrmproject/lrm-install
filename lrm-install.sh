24e424d89f9a126d14b1a562e3d4ce591e3fad5b
LRMcoin <45298379+lrmproject@users.noreply.github.com>
2018-11-28 00:11:50 +0300

    Update lrm-install.sh

--- lrm-install.sh
+++ lrm-install.sh
@@ -4,7 +4,7 @@ CONFIG_FILE='lrmcoin.conf'
 CONFIGFOLDER='/root/.lrmcoin'
 COIN_DAEMON='/usr/local/bin/lrmcoind'
 COIN_CLI='/usr/local/bin/lrmcoin-cli'
-COIN_REPO='https://github.com/lrmproject/LRMcoin/releases/download/v1.0.0.0/lrmcoin-1.0.0-x86_64-linux-gnu.tar.gz'
+COIN_REPO='https://github.com/lrmproject/LRMcoin/releases/download/v1.0.1.0/lrmcoin-1.0.1-x86_64-linux-gnu.tar.gz'
 COIN_NAME='LRMcoin'
 COIN_PORT=17273
 
